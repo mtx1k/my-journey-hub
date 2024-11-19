a1e40114a6b8a390bed44b4b50bf38ed3e168a9e
Vitalii <vitaliy.shekhovtsov@gmail.com>
2024-11-19 21:19:22 +0100

    Initial commit

---
+++ src/main/java/com/shekhovtsov/my_journey_hub/MyJourneyHubApplication.java
@@ -0,0 +1,13 @@
+package com.shekhovtsov.my_journey_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJourneyHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJourneyHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shekhovtsov/my_journey_hub/MyJourneyHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shekhovtsov.my_journey_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJourneyHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
